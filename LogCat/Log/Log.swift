057c16b078c7526c5c115c311609d80f512f1a30
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-08-18 10:16:30 -0300

    Initial Project

---
+++ LogCat/Log/Log.swift
@@ -0,0 +1,16 @@
+//
+//  Log.swift
+//  LogCat
+//
+//  Created by Vinicius Mangueira on 18/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import Foundation
+
+
+class Log {
+    class func d<T>(_ object: T) {
+        print(object)
+    }
+}
---
+++ LogCat/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LogCat
+//
+//  Created by Vinicius Mangueira on 18/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        Log.d(1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
